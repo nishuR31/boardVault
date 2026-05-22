e7b2829930bb092c28547f8be435ffb3057d1017
nishuR31 <nishangithub@gmail.com>
2026-05-22 10:39:58 +0530

    kuch toh kiya hu

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'app/index.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    usePathUrlStrategy();
+  }
   await dotenv.load(fileName: '.env');
   runApp(const App());
 }
