7895678b5d26b650aaafa2c065a38783d2a0a0a2
nishuR31 <nishangithub@gmail.com>
2026-05-22 16:17:11 +0530

    kuch toh kiya hu

--- lib/config/app_env.dart
+++ lib/config/app_env.dart
@@ -1,6 +1,6 @@
 class AppEnv {
-  static const backendUrl = String.fromEnvironment(
+  static final backendUrl = String.fromEnvironment(
     'BACKEND',
     defaultValue: 'http://localhost:3030',
-  );
+  ).trim();
 }
